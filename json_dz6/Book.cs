66183dcb601eb79452355a5ee39ab09e4b094b7f
Anastasiia <nastyagolova1@gmail.com>
2023-10-23 12:17:15 +0300

    DZ6

--- json_dz6/Book.cs
+++ json_dz6/Book.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace json_dz6
 {
     internal class Book
     {
+        //[JsonIgnore] 
         public int PublishingHouseId { get; set; }
+
+        //[JsonPropertyName("Name")] 
         public string? Title { get; set; }
         public PublishingHouse? PublishingHouse { get; set; }
     }
